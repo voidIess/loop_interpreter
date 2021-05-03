088b97e3f0a84f04b005f82d11fb06db0d1a34c6
voidless <juliusjedermann@gmail.com>
2021-05-03 17:55:11 +0200

    added cli interface

--- main.py
+++ main.py
@@ -9,32 +9,48 @@ def read_variable(name: str) -> int:
         return 0
 
 
-def write_varible(name, value):
+def write_variable(name, value):
     state[name] = value
 
+def parse_variable(expression:str):
+    if re.match("\w+=[0-9]+", expression):
+        name = re.sub("(\w+)=[0-9]+","\\1", expression)
+        value = int(re.sub("\w+=([0-9]+)","\\1", expression))
+        write_variable(name, value)
+    else:
+        print("Invalid Argument: {}".format(expression))
+        exit(1)
 
-def call(pc):
+
+def call(pc) -> int:
     current_line = lines[pc]
     if re.match("\w+:=\w+[+,-][0-9]+", current_line):
-        print("value assignment")
         # get all 3 varying expressions
         rhs = re.sub("\w+:=(\w+)[+,-][0-9]+", "\\1", current_line)
         lhs = re.sub("(\w+):=\w+[+,-][0-9]+", "\\1", current_line)
         summand = int(re.sub("\w+:=\w+([+,-][0-9]+)", "\\1", current_line))
 
         # do the desired calculation
-        write_varible(lhs, max(read_variable(rhs) + summand, 0))
+        write_variable(lhs, max(read_variable(rhs) + summand, 0))
+        return pc + 1
 
 
     elif re.match("LOOP \w+ DO", current_line):
-        print("LOOP Header")
+        repetitions = read_variable(re.sub("LOOP (\w+) DO", "\\1", current_line))
 
         inner_limit = pc
-        while (lines[inner_limit+1] != "END"):
+        while (lines[inner_limit] != "END"):
             inner_limit += 1
 
-        for i in range(pc + 1, inner_limit + 1):
-            call(i)
+        # execute the inner commands 'repetitions' times
+        for i in range(repetitions):
+            next_instruction = pc + 1
+            while (lines[next_instruction] != "END"):
+                next_instruction = call(next_instruction)
+
+        # return after the "END" statement
+        return inner_limit + 1
+
 
     else:
         print("SyntaxError in line {}:".format(pc))
@@ -48,12 +64,16 @@ if __name__ == '__main__':
         lines = file_dc.read().split("\n")
         file_dc.close()
 
-    # all our variables
-    state = {
-        "x": 5,
-        "y": 3
-    }
-    print(lines)
+    # initialise all variables
+    state = {}
+    for i in range(2, len(sys.argv)):
+        parse_variable(sys.argv[i])
+
     print(state)
-    call(0)
-    print(state)
+
+    program_length = len(lines)
+    next_instruction = 0
+    while (next_instruction < program_length):
+        next_instruction = call(next_instruction)
+
+    print("Result: {}".format(read_variable("z")))
